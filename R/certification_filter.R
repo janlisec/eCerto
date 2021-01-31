6065377ec51f456f409090c0b2d0f8e08b2d933e
fkress <frederik.kress@bam.de>
2021-01-31 19:40:33 +0100

    added first (numb) filter

---
+++ R/certification_filter.R
@@ -0,0 +1,119 @@
+# FILTER MODULE UI ----------------------------------------------------
+
+#' Title
+#'
+#' @param id 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+filterUI = function(id){
+  
+  fluidRow(
+    column(3, 
+           tags$div(title="Select an analyte (e.g. SI for Silicon)",
+                    selectInput(
+                      inputId = NS(id,"sel_analyt"),
+                      label = "analyte",
+                      choices = NULL, #levels(d()[, "analyte"]),
+                      selected = NULL #levels(d()[, "analyte"])[1]
+                    )
+                    #uiOutput(NS(id,"sel_analyt_ui"))
+           )
+    ),
+    column(3, 
+           tags$div(title="Filter samples by ID",
+                    
+                    selectizeInput(
+                      inputId = NS(id,"flt_samples"),
+                      label = "Filter Sample IDs",
+                      choices = NULL,
+                      selected = NULL,
+                      multiple = TRUE
+                    )
+                    #uiOutput(NS(id,"flt_samples"))
+           )
+    ),
+    column(
+      3,
+      numericInput(
+        inputId = NS(id,"precision"),
+        label = "Precision",
+        value = 4
+      )
+    ),
+  )
+  # column(
+  #   3,
+  #   selectInput(
+  #     inputId = "opt_show_files",
+  #     label = "Data view",
+  #     choices = c("none", "kompakt", "standard", "boxplot"),
+  #     selected = "none"
+  #   )
+  # )
+  
+  
+  
+  
+  
+}
+
+#' Title
+#'
+#' @param id 
+#' @param d 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+filterServer = function(id, d) {
+  stopifnot(is.reactive(d))
+  moduleServer(id, function(input, output, session){
+    
+    observeEvent(d(),{
+      updateSelectInput(
+        inputId ="sel_analyt",
+        label = "analyte",
+        choices = levels(d()[, "analyte"]),
+        selected = levels(d()[, "analyte"])[1]
+      )
+    })
+    
+    observeEvent(input$sel_analyt,{
+      #output$flt_samples <- renderUI({
+      #req(input$sel_analyt)
+      print(d())
+      choices <- d()[d()[, "analyte"] == input$sel_analyt, "ID"]
+      selected <-
+        choices[which(d()[d()[, "analyte"] == input$sel_analyt, "S_flt"])]
+      updateSelectizeInput(
+        inputId = "flt_samples",
+        label = "Filter Sample IDs",
+        choices = choices,
+        selected = selected
+      )
+      #})
+    }, ignoreInit = TRUE)
+    
+    reactive({
+      list(
+        analyte = reactive({input$sel_analyt}),
+        id = reactive({input$flt_samples})
+      )
+    })
+    
+    
+    # precision:
+    # n <- input$precision
+    # return(data.frame(
+    #   data[, 1, drop = F],
+    #   round(data[, -1, drop = F], digits = n),
+    #   "mean" = round(apply(data[, -1, drop = F], 1, mean, na.rm = T), digits = n),
+    #   "sd" = round(apply(data[, -1, drop = F], 1, sd, na.rm = T), digits = n)
+    # ))
+    
+  })
+}
