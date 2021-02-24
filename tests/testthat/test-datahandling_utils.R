132dcdc46c2d4d017cd7d5c6e7ce3d8284b632e2
fkress <frederik.kress@bam.de>
2021-02-24 10:46:43 +0100

    first tests

---
+++ tests/testthat/test-datahandling_utils.R
@@ -0,0 +1,14 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("loading functions decline other filetypes as excel", {
+  # Create sample data
+  df <- tibble::tibble(x = 1, y = 2)
+  path_csv <- tempfile()
+  write.csv(df, path_csv, row.names = FALSE)
+
+  fpath = paste0(path_csv,"test.csv")
+  expect_error(load_sheetnames(fpath))
+  expect_error(load_excelfiles(fpath, 1))
+})
---
+++ tests/testthat/test-xlsx_modules.R
@@ -0,0 +1,9 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+shiny::testServer(
+  app = .uploadTabsetsServer, {
+    session$setInputs
+  }
+)
