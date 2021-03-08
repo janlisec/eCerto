0122369982e61a081be97c85823cb2a8103eca2a
fkress <frederik.kress@bam.de>
2021-03-08 09:09:42 +0100

    indents and comments

--- R/analyte_module.R
+++ R/analyte_module.R
@@ -9,6 +9,7 @@
 #'
 #' @examples
 .analyteModuleUI = function(id){
+  # empty tabset panel, to be filled by the analytes in the server Module
   wellPanel(tabsetPanel(id = NS(id,"tabs")))
 }
 
@@ -27,39 +28,40 @@
    
     ns <- session$ns # to get full namespace here in server function
     analytes = isolate(reactiveValuesToList(analytelist))$analytes
+    # append/prepend a tab for each analyte available
     for (a.name in names(analytes)) {
-      # a.name = a$analytename
       prependTab(inputId = "tabs", select = TRUE,
           tabPanel(
             title=a.name, 
             fluidRow(
               column(6,
-                     # tags$div(
-                       # title="Filter samples by ID",
-                       selectizeInput(
-                         inputId = ns(paste0("flt_samples",a.name)),#NS(id,paste0("flt_samples",a.name)),
-                         label = "Filter Sample IDs",
-                         choices = analytelist$analytes[[a.name]]$sample_ids,
-                         selected = analytelist$analytes[[a.name]]$sample_filter,
-                         multiple = TRUE
-                       )
-                     # )
+               selectizeInput(
+                 inputId = ns(paste0("flt_samples",a.name)),#NS(id,paste0("flt_samples",a.name)),
+                 label = "Filter Sample IDs",
+                 choices = analytelist$analytes[[a.name]]$sample_ids,
+                 selected = analytelist$analytes[[a.name]]$sample_filter,
+                 multiple = TRUE
+               )
               ),
               column(6,
-                     numericInput(
-                       inputId =ns(paste0("precision",a.name)),
-                       label = "Precision",
-                       value = 4
-                     )
+               numericInput(
+                 inputId =ns(paste0("precision",a.name)),
+                 label = "Precision",
+                 value = 4
+               )
               ),
             ),
           )
       )}
     
+    # change the "selected tab" reactive in the reactiveValues when another tab
+    # is selected
     observeEvent(input$tabs,{
       analytelist$selected_tab = input$tabs
     })
     
+    # update precision and the selected sample id filter in the reactiveValues
+    # when their value change in the selected tab
     observe({
       lapply(names(analytes), function(i){
         analytelist$analytes[[i]]$precision = input[[paste0("precision",i)]]
