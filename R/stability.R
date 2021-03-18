d717df281aa6f4bee1b0d4a16bbadfb168fc08e3
fkress <frederik.kress@bam.de>
2021-03-18 08:06:27 +0100

    stability module framework added

---
+++ R/stability.R
@@ -0,0 +1,11 @@
+# STABILITY MODULE -------------------------
+
+.StabilityUI = function(id) {
+
+}
+
+.StabilityServer = function(id, rv) {
+  moduleServer(id, function(input, output, session) {
+   
+  })
+}
