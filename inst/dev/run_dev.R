a002d95333c2eab694c57f9b1c8c7cf7880ec1df
Lisec <jan.lisec@bam.de>
2021-09-28 17:56:55 +0200

    Diffficult to maintain local and online App in parallel. To make this easier inst/Rmd folder was pushed to inst/app/www instead.

--- inst/dev/run_dev.R
+++ inst/dev/run_dev.R
@@ -4,7 +4,6 @@ suppressWarnings(lapply(paste('package:', names(sessionInfo()$otherPkgs), sep = 
 rm(list = ls(all.names = TRUE)) # clean environment
 devtools::document('.') # create NAMESPACE and man
 devtools::load_all('.') # load package
-#rmarkdown::render(fnc_get_local_file("help_start.Rmd", copy_to_tempdir = FALSE), quiet = TRUE)
+rmarkdown::render(fnc_get_local_file("help_start.Rmd", copy_to_tempdir = FALSE), quiet = TRUE)
 options(app.prod = FALSE) # TRUE = production mode, FALSE = development mode
 shiny::runApp('inst/app') # run the main app
-
