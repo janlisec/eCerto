2b9f1f0405f8e65fd96f74a477dca5d0b9d4c0b2
Lisec <jan.lisec@bam.de>
2022-12-15 13:42:49 +0100

    Better tests for the LTS plot function.

--- R/fnc_plot_lts_data.R
+++ R/fnc_plot_lts_data.R
@@ -11,73 +11,68 @@
 #'
 #' @noRd
 #' @keywords internal
-plot_lts_data <- function(x=NULL, type=1) {
-
+plot_lts_data <- function(x = NULL, type = 1) {
   # date estimation is approximate (based on ~30d/month or precisely on 365/12=30.42)
   days_per_month <- 30.4167
 
   # ensure that data is ordered after time
-  x[["val"]] <- x[["val"]][order(x[["val"]][,"Date"]),]
+  x[["val"]] <- x[["val"]][order(x[["val"]][, "Date"]), ]
 
   # get specific data
-  vals <- x[["val"]][,"Value"]
-  rt <- x[["val"]][,"Date"]
-  mon <- calc_time_diff(rt, type="day")/days_per_month
+  vals <- x[["val"]][, "Value"]
+  rt <- x[["val"]][, "Date"]
+  mon <- calc_time_diff(rt, type = "day") / days_per_month
 
   # establish linear model
-  foo.lm <- stats::lm(vals~mon)
+  foo.lm <- stats::lm(vals ~ mon)
   a <- stats::coef(foo.lm)[1]
   b <- stats::coef(foo.lm)[2]
 
   # extract relevant values from definition part
-  U <- x[["def"]][,"U"]
-  mn <- x[["def"]][,"CertVal"]
-  ylab <- paste0(x[["def"]][,"KW_Def"], ifelse(is.na(x[["def"]][,"KW"]), "", paste0(" (",x[["def"]][,"KW"],")")), " [", x[["def"]][,"KW_Unit"],"]")
-  main <- x[["def"]][,"KW"]
-  sub <- x[["def"]][,"U_Def"]
+  U <- x[["def"]][, "U"]
+  mn <- x[["def"]][, "CertVal"]
+  ylab <- paste0(x[["def"]][, "KW_Def"], ifelse(is.na(x[["def"]][, "KW"]), "", paste0(" (", x[["def"]][, "KW"], ")")), " [", x[["def"]][, "KW_Unit"], "]")
+  main <- x[["def"]][, "KW"]
+  sub <- x[["def"]][, "U_Def"]
 
   # correct values by coef estimate
-  foo_adj <- vals-(a-mn)
-  foo_lts <- ceiling(abs(U/b))
+  foo_adj <- vals - (a - mn)
+  foo_lts <- ceiling(abs(U / b))
 
-  # color comment datapoints differently
-  if ("Comment" %in% colnames(x[["val"]])) com <- x[["val"]][,"Comment"] else com <- rep(NA, nrow(x[["val"]]))
-
-  if (type==1) {
-    # generate 'real time window' plot
-    plot(
-      vals~mon,
-      type="n",
-      ylim=range(c(vals,mn+c(-1,1)*U), na.rm=T),
-      xlab="Month [n]",
-      ylab=ylab,
-      sub=sub,
-      main=main
-    )
-    graphics::axis(side = 3, at = range(mon), labels = rt[c(1,length(rt))])
-    graphics::abline(foo.lm, lty=2, col=4) # <-- slope
-    graphics::abline(h=mn+c(-1,0,1)*U, lty=c(2,1,2), col=c(3,2,3))
-    graphics::points(vals~mon, pch=24, bg=c(grDevices::grey(0.6),2)[1+!is.na(com)])
+  # color comment data points differently
+  if ("Comment" %in% colnames(x[["val"]])) {
+    com <- x[["val"]][, "Comment"]
+  } else {
+    com <- rep(NA, nrow(x[["val"]]))
   }
 
-  if (type==2) {
-    # generate 'fake time window' plot
+  # generate 'real time window' plot
+  if (type == 1) {
     plot(
-      c(foo_adj,mn+b*foo_lts)~c(mon,foo_lts),
-      pch=21,
-      bg=c(c(grDevices::grey(0.6),2)[1+!is.na(com)],4),
-      ylim=range(c(foo_adj,mn+b*foo_lts,mn+c(-1,1)*U)),
-      xlab="Month [n]",
-      ylab=paste(ylab, "adjusted"),
-      sub=sub,
-      main=paste(main, "(adjusted)")
+      vals ~ mon, type = "n",
+      ylim = range(c(vals, mn + c(-1, 1) * U), na.rm = T),
+      xlab = "Month [n]", ylab = ylab, sub = sub, main = main
     )
-    graphics::axis(side = 3, at = c(0, foo_lts), labels = c(rt[1], rt[1]+foo_lts*days_per_month))
-    graphics::abline(stats::lm(foo_adj~mon), lty=2, col=4)
-    graphics::abline(h=mn+c(-1,0,1)*U, lty=c(2,1,2), col=c(3,2,3))
-    graphics::text(x=foo_lts, y=mn+b*foo_lts, pos=2, labels = paste("n =",foo_lts))
+    graphics::axis(side = 3, at = range(mon), labels = rt[c(1, length(rt))])
+    graphics::abline(foo.lm, lty = 2, col = 4) # <-- slope
+    graphics::abline(h = mn + c(-1, 0, 1) * U, lty = c(2, 1, 2), col = c(3, 2, 3))
+    graphics::points(vals ~ mon, pch = 24, bg = c(grDevices::grey(0.6), 2)[1 + !is.na(com)])
   }
 
-  names(foo_lts) <- as.character(as.POSIXlt(as.Date(rt[1]+foo_lts*days_per_month, origin="1900-01-01")))
+  # generate 'fake time window' plot
+  if (type == 2) {
+    plot(
+      c(foo_adj, mn + b * foo_lts) ~ c(mon, foo_lts),
+      pch = 21, bg = c(c(grDevices::grey(0.6), 2)[1 + !is.na(com)], 4),
+      ylim = range(c(foo_adj, mn + b * foo_lts, mn + c(-1, 1) * U)),
+      xlab = "Month [n]", ylab = paste(ylab, "adjusted"), sub = sub, main = paste(main, "(adjusted)")
+    )
+    graphics::axis(side = 3, at = c(0, foo_lts), labels = c(rt[1], rt[1] + foo_lts * days_per_month))
+    graphics::abline(stats::lm(foo_adj ~ mon), lty = 2, col = 4)
+    graphics::abline(h = mn + c(-1, 0, 1) * U, lty = c(2, 1, 2), col = c(3, 2, 3))
+    graphics::text(x = foo_lts, y = mn + b * foo_lts, pos = 2, labels = paste("n =", foo_lts))
+  }
+
+  names(foo_lts) <- as.character(as.POSIXlt(as.Date(rt[1] + foo_lts * days_per_month, origin = "1900-01-01")))
   invisible(foo_lts)
 }
--- tests/testthat/test-fnc_plot_lts_data.R
+++ tests/testthat/test-fnc_plot_lts_data.R
@@ -1,24 +1,41 @@
+s <- eCerto:::test_Stability_Excel()
+apm <- list("Mn"=list("confirmed"=TRUE))
+mt <- data.frame("analyte"="Mn", "cert_val"=1, "U_abs"=1, "sd"=1, "unit"="unit")
+
 testthat::test_that("plot_lts_data works", {
-  s <- eCerto:::test_Stability_Excel()
-  apm <- list("Mn"=list("confirmed"=TRUE))
   x_prep <- eCerto:::prepFigS1(s = s, an = "Mn", apm=apm)
-  out <- eCerto:::plot_lts_data(x=x_prep)
+  out <- eCerto:::plot_lts_data(x = x_prep, type = 0)
   testthat::expect_true(is.numeric(out))
   testthat::expect_equal(names(out), "2184-02-22")
   testthat::expect_equal(unname(out), 2076)
-  mt <- data.frame("analyte"="Mn", "cert_val"=1, "U_abs"=1, "sd"=1, "unit"="unit")
   x_prep <- eCerto:::prepFigS1(s = s, an = "Mn", apm=apm, U_Def="U", mt=mt)
   vdiffr::expect_doppelganger(
     title = "FigS1 type1",
     fig = function() eCerto:::plot_lts_data(x=x_prep, type = 1)
   )
+  out2 <- eCerto:::plot_lts_data(x=x_prep, type = 2)
+  testthat::expect_equal(names(out2), "2135-01-04")
   vdiffr::expect_doppelganger(
     title = "FigS1 type2",
     fig = function() eCerto:::plot_lts_data(x=x_prep, type = 2)
   )
   x_prep[["def"]][,"KW"] <- "test_text"
+  x_prep[["val"]][,"Comment"] <- c(rep(NA, 51), "test_text")
   vdiffr::expect_doppelganger(
     title = "FigS1 type1 with KW",
     fig = function() eCerto:::plot_lts_data(x=x_prep, type = 1)
   )
+})
+
+testthat::test_that("plot_lts_data type=2 works", {
+  x_prep <- eCerto:::prepFigS1(s = s, an = "Mn", apm=apm, U_Def="U", mt=mt)
+  out2 <- eCerto:::plot_lts_data(x=x_prep, type = 2)
+  testthat::expect_equal(names(out2), "2135-01-04")
+})
+
+testthat::test_that("plot_lts_data Comment points are colored", {
+  x_prep <- eCerto:::prepFigS1(s = s, an = "Mn", apm=apm, U_Def="U", mt=mt)
+  x_prep[["val"]][,"Comment"] <- c(rep(NA, 51), "test_text")
+  out3 <- eCerto:::plot_lts_data(x=x_prep, type = 0)
+  testthat::expect_equal(names(out3), "2135-01-04")
 })
