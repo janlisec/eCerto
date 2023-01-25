7f96b2daaedbff17d607e8c7d4e4e0c3c85ae2d0
Lisec <jan.lisec@bam.de>
2023-01-25 13:29:38 +0100

    Fix an error in case that user provides H data with `H_type` column containing empty cells.

--- R/fnc_checkHdata.R
+++ R/fnc_checkHdata.R
@@ -13,11 +13,15 @@ checkHdata <- function(x) {
   # rename if if first column is not named 'analyte' and convert to factor
   colnames(x)[1] <- "analyte"
   x[,"analyte"] <- factor(x[,"analyte"])
-  # ensure that there is a second column 'H_type' and convert to factor
+  # ensure that there is a second column 'H_type' and convert to factor with at least one level
   if (colnames(x)[2]!="H_type" && colnames(x)[3]=="value") {
-    x <- cbind(x[,1,drop=FALSE], data.frame("H_type"=gl(n = 1, k = nrow(x), labels = "hom")), x[,2:ncol(x)])
+    x <- cbind(x[,"analyte",drop=FALSE], data.frame("H_type"=gl(n = 1, k = nrow(x), labels = "hom")), x[,2:ncol(x)])
   } else {
     x[,"H_type"] <- factor(x[,"H_type"])
+    # ensure that H_type has one level (because downstream functions fail otherwise)
+    if (length(levels(x[,"H_type"]))==0) {
+      x[,"H_type"] <- gl(n = 1, k = nrow(x), labels = "hom")
+    }
   }
   # ensure that there is a third column 'Flasche' and convert to factor
   colnames(x)[3] <- "Flasche"
