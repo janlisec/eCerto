3d45dc623c2e046b1ea8f9d44c88b6b154641c2f
Lisec <jan.lisec@bam.de>
2023-07-31 14:48:44 +0200

    Test for prepTabC1

--- tests/testthat/test-fnc_prepTabC2.R
+++ tests/testthat/test-fnc_prepTabC2.R
@@ -6,5 +6,10 @@ testthat::test_that(
     suppressWarnings( {out <- eCerto:::prepTabC2(dat = x)} )
     testthat::expect_true(is.data.frame(out))
     testthat::expect_equal(out[,"Median"], 1.4936)
+    x2 <- x
+    x2[,"Lab"] <- gsub("L2", "L4", gsub("L1", "L3", x2[,"Lab"]))
+    x2[,"value"] <- sample(x2[,"value"])
+    x3 <- rbind(x, x2)
+    eCerto:::prepTabC2(dat = x3, excl_labs = TRUE)
   }
 )
