cedc53109bf4a5cd79d9ae6d76e903bf28884d3c
Lisec <jan.lisec@bam.de>
2023-08-08 18:43:31 +0200

    Test for Tab.C2 updated (rounding is now achieved only in style function and omitted in prep function.

--- tests/testthat/test-fnc_prepTabC2.R
+++ tests/testthat/test-fnc_prepTabC2.R
@@ -3,13 +3,17 @@ testthat::test_that(
   code = {
     x <- eCerto:::test_Certification_Excel()
     # Anscombe-Test returns a warning for this test data
-    suppressWarnings( {out <- eCerto:::prepTabC2(dat = x)} )
+    suppressWarnings({
+      out <- eCerto:::prepTabC2(dat = x)
+    })
     testthat::expect_true(is.data.frame(out))
-    testthat::expect_equal(out[,"Median"], 1.4936)
+    testthat::expect_equal(out[,"Median"], 1.4935875, tolerance = 8)
+    testthat::expect_equal(out[,"SD"], 0.003152518, tolerance = 8)
     x2 <- x
     x2[,"Lab"] <- gsub("L2", "L4", gsub("L1", "L3", x2[,"Lab"]))
     x2[,"value"] <- sample(x2[,"value"])
     x3 <- rbind(x, x2)
-    eCerto:::prepTabC2(dat = x3, excl_labs = TRUE)
+    out <- eCerto:::prepTabC2(dat = x3, excl_labs = TRUE)
+    testthat::expect_equal(out[,"SD"], 0.002521652, tolerance = 8)
   }
 )
