eff5e24013b0aacc2472aec4c1e08fccecafe0dd
Lisec <jan.lisec@bam.de>
2023-09-15 16:16:51 +0200

    v0.2.4

--- R/fnc_prepTabS1.R
+++ R/fnc_prepTabS1.R
@@ -13,16 +13,20 @@ prepTabS1 <- function(x) {
   message("[prepTabS1] perform statistics on imported stability data")
   stopifnot(all(c("analyte", "Value", "Date") %in% colnames(x)))
   plyr::ldply(split(x, x[,"analyte"]), function(x) {
+    #x[,"Date"] <- calc_time_diff(x[,"Date"], type = "mon", exact=TRUE)
     x_lm <- stats::lm(Value ~ Date, data=x)
     mon_diff <- max(calc_time_diff(x[,"Date"], type = "mon"))
-    x_slope <- summary(x_lm)$coefficients[2,1:2]
-    # according to B.3.4 from ISO Guide 35
-    p_val <- 2 * stats::pt(abs(x_slope[1]/x_slope[2]), df = stats::df.residual(x_lm), lower.tail = FALSE)
+    x_coef <- unname(summary(x_lm)$coefficients["Date",])
+    # according to B.3.4 from ISO Guide 35 which is similar to summary(lm))coef[4]
+    #p_val <- 2 * stats::pt(abs(x_coef[1]/x_coef[2]), df = stats::df.residual(x_lm), lower.tail = FALSE)
+    #p_val <- x_coef[4]
+    #browser()
     data.frame(
       "mon_diff"=mon_diff,
-      "slope"=x_slope[1],
-      "SE_slope"=x_slope[2],
-      "u_stab"=abs(x_slope[1]*x_slope[2]),
-      "P"=p_val)
+      "slope"=x_coef[1],
+      "SE_slope"=x_coef[2],
+      "u_stab"=abs(x_coef[1]*x_coef[2]),
+      "P"=x_coef[4]
+    )
   }, .id="analyte")
 }
