d897f5e406f4d6ad52671b1b8835e061262d7ec6
Lisec <jan.lisec@bam.de>
2023-09-27 14:33:03 +0200

    Correct documentation for `styleTabH2`.

--- R/fnc_styleTabH2.R
+++ R/fnc_styleTabH2.R
@@ -1,13 +1,10 @@
 #'@title fnc_styleTabH2.
 #'@description \code{styleTabH2} will style Tab.H2 for pretty output.
 #'@details tbd.
-#'@param x The Hom data from an session R6 object.
-#'@param mt The mt from an session R6 object.
-#'@param apm The apm from an session R6 object.
-#'@param output Return either the dataframe with styling information in columns or the corresponding datatable object.
-#'@param cr Current row selected (relevant if output = 'dt').
+#'@param x data.frame.
+#'@param precision Rounding precision.
 #'@examples
-#'x <- data.frame("Flasche"=LETTERS[1:3], "mean"=1:3, "sd" = rnorm(3), "n" = 4:6)
+#'x <- data.frame("Item"=LETTERS[1:3], "mean"=1:3, "sd" = rnorm(3), "n" = 4:6)
 #'eCerto:::styleTabH2(x = x)
 #'eCerto:::styleTabH2(x = x, precision = 2)
 #'@return A datatable object.
@@ -16,7 +13,7 @@ styleTabH2 <- function(x, precision = 4) {
   message("[styleTabH2] styling Tab.H2")
   cols <- which(colnames(x) %in% c("mean","sd"))
   for (i in cols) x[,i] <- pn(x[,i], precision)
-  # # attach a blank column at the end
+  # attach a blank column at the end
   x <- cbind(x, data.frame(" "=" ", check.names = FALSE))
   dt <- DT::datatable(
     data = x,
