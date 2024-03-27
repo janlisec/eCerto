5a26a583b1f6e60bd301cfeeff9704e9378f9af8
janlisec <jan.lisec@bam.de>
2024-03-27 17:23:15 +0100

    ExcelUpload module stripped from `shinyalert` calls and improved using `assert_col` function.

--- R/m_ExcelUpload.R
+++ R/m_ExcelUpload.R
@@ -39,16 +39,16 @@ m_ExcelUpload_UI <- function(id) {
       shiny::fluidRow(
         style = "background-color: #f5f5f5; border: 1px; border-radius: 4px; border-color: #e3e3e3; border-style: solid; margin: 0px; padding-top: 6px",
         shiny::div(
-          style = "width: 260px; float: left; margin-left: 15px;",
-          shiny::uiOutput(outputId = ns("inp_file"))
-        ),
-        shiny::div(
           style = "width: 130px; float: left; margin-left: 15px;",
           shiny::radioButtons(
             inputId = ns("moduleSelect"),
-            label = NULL,
+            label = "File format",
             choices = "dummy"
           )
+        ),
+        shiny::div(
+          style = "width: 260px; float: left; margin-left: 15px;",
+          shiny::uiOutput(outputId = ns("inp_file"))
         ),
         shiny::div(
           style = "width: 260px; float: left; margin-left: 15px;",
@@ -144,16 +144,6 @@ m_ExcelUpload_Server <- function(id, rv = NULL, msession = NULL) {
       current_file_input(input$excel_file)
     })
 
-    # sheetnumber <- shiny::reactive({
-    #   shiny::req(input$sheet_number)
-    #   switch (
-    #     exl_fmt(),
-    #     "Certification" = as.numeric(input$sheet_number),
-    #     "Homogeneity" = as.numeric(input$sheet_number),
-    #     "Stability" = 1:length(xlsxSheetNames(input$excel_file$datapath))
-    #   )
-    # })
-
     file_number <- shiny::reactive({
       shiny::req(input$file_name)
       which(input$excel_file$name %in% input$file_name)
@@ -163,7 +153,6 @@ m_ExcelUpload_Server <- function(id, rv = NULL, msession = NULL) {
     rv_xlsx_range_select <- m_xlsx_range_select_Server(
       id = "rng_select",
       current_file_input = current_file_input,
-      #sheet = sheetnumber,
       sheet = shiny::reactive({ as.numeric(input$sheet_number) }),
       file = file_number,
       excelformat = exl_fmt
@@ -172,130 +161,98 @@ m_ExcelUpload_Server <- function(id, rv = NULL, msession = NULL) {
     # initialize return object 'out'
     out <- shiny::reactiveValues(data = NULL, input_files = NULL)
 
-    # when LOAD Button is clicked
-    shiny::observeEvent(input$btn_load, {
-      # Append File column
-      message("[m_ExcelUpload] Load-button clicked")
-      req(rv_xlsx_range_select$tab)
+    # load from Excel
+    load_from_excel <- function() {
+      load_result <- NULL
       tab_flt <- rv_xlsx_range_select$tab
+      # Append File column
       out$input_files <- current_file_input()$name
       # perform minimal validation checks
       if (exl_fmt()=="Homogeneity") {
-        tab_flt <- tab_flt[[1]]
-        tab_flt[["File"]]<- rep(current_file_input()$name[1], nrow(tab_flt))
-        if (!"analyte" %in% colnames(tab_flt)) message("m_ExcelUpload_Server: observeEvent(input$btn_load): No column 'analyte' found in input file.")
-        if (!"value" %in% colnames(tab_flt)) message("m_ExcelUpload_Server: observeEvent(input$btn_load): No column 'value' found in input file.")
-        if (!is.numeric(tab_flt[,"value"])) message("m_ExcelUpload_Server: observeEvent(input$btn_load): Column 'value' in input file contains non-numeric values.")
-        out$data <- tab_flt
+        x <- tab_flt[[1]]
+        x <- checkHdata(x)
+        x[,"File"] <- rep(current_file_input()$name[1], nrow(x))
+        load_result <- x
       } else if (exl_fmt() == "Certification") {
         if (!silent) message("[m_ExcelUpload_Server] Load Certification data")
+        # append file info
         for (i in 1:length(tab_flt)) {
-          tab_flt[[i]][["File"]] = rep(current_file_input()$name[i], nrow(tab_flt[[i]]))
+          tab_flt[[i]][["File"]] <- rep(current_file_input()$name[i], nrow(tab_flt[[i]]))
         }
-        error_modal <- function() {
-          # perform minimal validation tests
-          if (!length(tab_flt)>=2) message("m_ExcelUpload_Server: observeEvent(input$btn_load): Less than 2 laboratory files uploaded. Please select more files!")
-          # Try-Catch any errors during upload and open a modal window if so
-          results = tryCatch({
-            expr = prepTabC0(df_list = tab_flt)
-          },
-          error = function(errormessage) {
-            message("Excel-Upload: Error occured: ", errormessage)
-            shiny::showModal(
-              shiny::modalDialog(
-                title = "Something went wrong with Upload.",
-                paste(
-                  "Check for example the selected rows and columns\n",
-                  errormessage
-                )
-              )
-            )
-            return(NULL)
-          })
-          out$data = results
-        }
-        # in case (a) it is Certification module and (b) the input table has not
-        # been filtered, then ask if this is correct
-        if(ncol(rv_xlsx_range_select$tab_upload[[1]]) == ncol(rv_xlsx_range_select$tab[[1]]) &
-           nrow(rv_xlsx_range_select$tab_upload[[1]]) == nrow(rv_xlsx_range_select$tab[[1]])
-        ) {
-          if(!silent) message("[m_ExcelUpload] Forgot select row and column?")
-          shinyalert::shinyalert(
-            title = "Forgot to select a range?",
-            text = "You're trying to upload the complete table without limiting to a specific range. Are you sure to proceed?",
-            showCancelButton = TRUE,
-            showConfirmButton = TRUE,
-            callbackR = function(x) { if(x != FALSE) error_modal() }
-          )
-        } else {
-          error_modal()
-        }
-
+        # try to convert to data frame
+        tabC0 <- tryCatch(
+          expr = { prepTabC0(df_list = tab_flt) },
+          error = function(e) {
+            out <- tab_flt[[i]]
+            attr(out, "msg") <- e
+            return(out)
+          }
+        )
+        # in case (a) it is Certification module and (b) the input table has not been filtered, then ask if this is correct
+        test_selection <- ncol(rv_xlsx_range_select$tab_upload[[1]]) == ncol(rv_xlsx_range_select$tab[[1]]) & nrow(rv_xlsx_range_select$tab_upload[[1]]) == nrow(rv_xlsx_range_select$tab[[1]])
+        if (test_selection) { attr(tabC0, "msg") <- "Range specification is on default value" }
+        load_result <- tabC0
       } else if (exl_fmt() == "Stability") {
         # STABILITY data may come in 3 versions
         # (1) as simple two column format (Date, Value) with separate tables for each analyte
         # (2) as LTS format with a meta data header containing machine info, certification data etc.
         # (3) as a data frame giving 'Temp' info additionally to compute Arrhenius estimate of uncertainty
         test_format <- tab_flt[[as.numeric(input$sheet_number)]] # openxlsx::read.xlsx(xlsxFile = input$s_input_file$datapath[1], sheet = 1)
-        if (ncol(test_format)>=4) {
+        if (ncol(test_format)<4) {
+          # (1) as simple two column format (Date, Value) with separate tables for each analyte
+          sheetnames <- xlsxSheetNames(input$excel_file$datapath[1])
+          s_dat <- plyr::ldply(1:length(sheetnames), function(x) {
+            cbind("analyte"= sheetnames[x], tab_flt[[x]])
+          })
+        } else {
           if ("KW" %in% colnames(test_format)) {
             # (2) as LTS format with a meta data header containing machine infos, certification data etc.
-            s_dat <- read_lts_input(
-              file = input$excel_file$datapath[1],
-              simplify=TRUE)
+            s_dat <- read_lts_input(file = input$excel_file$datapath[1], simplify=TRUE)
             colnames(s_dat)[colnames(s_dat)=="KW"] <- "analyte"
           } else {
             # (3) as a dataframe giving Temp info additionally to compute Arrhenius estimate of uncertainty
             s_dat <- tab_flt[[as.numeric(input$sheet_number)]]
-            cns <- c("analyte","Value","Date","Temp")
-            if (!all(cns %in% colnames(s_dat))) {
-              shinyalert::shinyalert(
-                title = "Error in data upload",
-                text = paste0("Required column(s) '", paste(cns[!(cns %in% colnames(s_dat))], collapse="', '"), "' not found in input file."),
-                showCancelButton = FALSE,
-                showConfirmButton = TRUE,
-                callbackR = function(x) { s_dat <- NULL }
-              )
-            }
-            if ("Date" %in% colnames(s_dat)) s_dat[,"time"] <- as.numeric(s_dat[,"Date"]-min(s_dat[,"Date"]))
+            assert_col(df = s_dat, name = "Temp", type = "numeric")
           }
-        } else {
-          # (1) as simple two column format (Date, Value) with separate tables for each analyte
-          sheetnames <- xlsxSheetNames(input$excel_file$datapath[1])
-          s_dat <- plyr::ldply(
-            1:length(sheetnames),
-            #sheetnumber(),
-            function(x) {
-              cbind(
-                "analyte"= sheetnames[x],
-                tab_flt[[x]]
-                # "File" = current_file_input()$name
-              )
-            }
-          )
         }
-        cns <- c("analyte","Value","Date")
-        shiny::validate(shiny::need(
-          expr = all(cns %in% colnames(s_dat)),
-          message = paste("Require column(s)", paste(cns[!(cns %in% colnames(s_dat))], collapse=", "), "in input file.")
-        ))
-        shiny::validate(shiny::need(is.numeric(s_dat[,"Value"]), "Column 'Value' in input file contains non-numeric values."))
-        if (!inherits(s_dat[,"Date"], "Date")) { s_dat[,"Date"] <- as.Date.character(s_dat[,"Date"], tryFormats = c("%Y-%m-%d","%d.%m.%Y","%Y/%m/%d")) }
-        shiny::validate(shiny::need(inherits(s_dat[,"Date"], "Date"), "Sorry, could not convert column 'Date' into correct format."))
-        s_dat[,"analyte"] <- factor(s_dat[,"analyte"], levels = unique(s_dat[,"analyte"]))
-        out$data <- s_dat
+        assert_col(df = s_dat, name = "analyte", type = "factor")
+        assert_col(df = s_dat, name = "Value", type = "numeric")
+        assert_col(df = s_dat, name = "Date", type = "Date")
+        s_dat[,"time"] <- as.numeric(s_dat[,"Date"]-min(s_dat[,"Date"]))
+        load_result <- s_dat
+      }
+      return(load_result)
+    }
+
+    # when LOAD Button is clicked
+    shiny::observeEvent(input$btn_load, {
+      req(rv_xlsx_range_select$tab)
+      message("[m_ExcelUpload] Load-button clicked")
+      tmp <- try(load_from_excel())
+      if (inherits(tmp, "try-error") | !is.null(attr(tmp, "msg")) | is.null(tmp)) {
+        shinyWidgets::ask_confirmation(
+          inputId = "ignore_problems", btn_labels = c("Cancel upload", "Upload anyways"),
+          title = "Problems detected", type = "error", html=TRUE,
+          text = shiny::tagList(
+            shiny::div(style="text-align: left;",
+              shiny::HTML("<b>These messages were returned:</b><br>"),
+              shiny::div(style="font-size: 12px;", tags$div(tags$ul(lapply(attr(tmp, "msg"), tags$li)))),
+              shiny::HTML("<b>This would be the structure of the upload:</b><br>"),
+              shiny::div(style="font-size: 12px;", shiny::HTML(paste(utils::capture.output(utils::str(tmp)), collapse="<br>")))
+            )
+          )
+        )
+      } else {
+        out$data <- tmp
       }
     }, ignoreInit = TRUE)
 
-    # Action link for help on Excel format for upload
-    shiny::observeEvent(input$moduleUploadHelp, {
-      switch(
-        exl_fmt(),
-        "Certification" = show_help("certification_dataupload"),
-        "Homogeneity" = show_help("homogeneity_dataupload"),
-        "Stability" = show_help("stability_dataupload")
-      )
-    }, ignoreInit = TRUE)
+    shiny::observeEvent(input$ignore_problems, {
+      if (input$ignore_problems) {
+        tmp <- try(load_from_excel())
+        out$data <- tmp
+      }
+    })
 
     # when Excel was uploaded with LOAD-Button...
     shiny::observeEvent(out$data, {
@@ -314,6 +271,15 @@ m_ExcelUpload_Server <- function(id, rv = NULL, msession = NULL) {
     shiny::observeEvent(input$showHelp, {
       shiny::updateNavbarPage(session = msession, inputId = "navbarpage", selected = "tP_help")
     })
+    # Action link for help on Excel format for upload
+    shiny::observeEvent(input$moduleUploadHelp, {
+      switch(
+        exl_fmt(),
+        "Certification" = show_help("certification_dataupload"),
+        "Homogeneity" = show_help("homogeneity_dataupload"),
+        "Stability" = show_help("stability_dataupload")
+      )
+    }, ignoreInit = TRUE)
 
   })
 }
